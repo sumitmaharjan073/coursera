b9deca5342e45d4b05bc980bbbadaca2633c0066
Arjun Shrestha <bboyarjun07@gmail.com>
2020-12-18 14:57:12 +0545

    physics completed class 8

--- classes/class8/Maths.php
+++ classes/class8/Maths.php
@@ -71,49 +71,49 @@
 		
 		<div class="Rational_Numbers-Introduction show">
 			<?php
-			include 'Contentvariable.php';
+			include 'ContentVariable.php';
 			echo fread($myFile11,filesize("Content_Details/E0311.html"));
 			?>
 		</div>
 		<div class="Rational_Numbers-Types_of_numbers">
 			<?php
-			include 'Contentvariable.php';
+			include 'ContentVariable.php';
 			echo fread($myFile12,filesize("Content_Details/E0312.html"));
 			?>
 		</div>
 		<div class="Rational_Numbers-Properties_Closure">
 			<?php
-			include 'Contentvariable.php';
+			include 'ContentVariable.php';
 			echo fread($myFile13,filesize("Content_Details/E0313.html"));
 			?>
 		</div>
 		<div class="Rational_Numbers-Properties_Associativity">
 			<?php
-			include 'Contentvariable.php';
+			include 'ContentVariable.php';
 			echo fread($myFile14,filesize("Content_Details/E0314.html"));
 			?>
 		</div>
 		<div class="Rational_Numbers-Additive_and_Multiplicative">
 			<?php
-			include 'Contentvariable.php';
+			include 'ContentVariable.php';
 			echo fread($myFile15,filesize("Content_Details/E0315.html"));
 			?>
 		</div>
 		<div class="Rational_Numbers-Distributive_property">
 			<?php
-			include 'Contentvariable.php';
+			include 'ContentVariable.php';
 			echo fread($myFile16,filesize("Content_Details/E0316.html"));
 			?>
 		</div>
 		<div class="Rational_Numbers-Representation_of_number">
 			<?php
-			include 'Contentvariable.php';
+			include 'ContentVariable.php';
 			echo fread($myFile17,filesize("Content_Details/E0317.html"));
 			?>
 		</div>
 		<div class="Rational_Numbers-Two_Rational_numbers">
 			<?php
-			include 'Contentvariable.php';
+			include 'ContentVariable.php';
 			echo fread($myFile18,filesize("Content_Details/E0318.html"));
 			?>
 		</div>
@@ -121,99 +121,99 @@
 		
 		<div class="Linear_Equations_in_One_Variable-Introduction">
 		<?php
-			include 'Contentvariable.php';
+			include 'ContentVariable.php';
 			echo fread($myFile21,filesize("Content_Details/E0321.html"));
 			?>	
 		</div>
 		<div class="Linear_Equations_in_One_Variable-Algebraic_Equations_&_Solutions">
 		<?php
-			include 'Contentvariable.php';
+			include 'ContentVariable.php';
 			echo fread($myFile22,filesize("Content_Details/E0322.html"));
 			?>
 		</div>
 		<div class="Linear_Equations_in_One_Variable-Solving_Equations">
 		<?php
-			include 'Contentvariable.php';
+			include 'ContentVariable.php';
 			echo fread($myFile23,filesize("Content_Details/E0323.html"));
 			?>
 		</div>
 		<div class="Linear_Equations_in_One_Variable-Solving_Equations_having_variables_on_both_sides">
 		<?php
-			include 'Contentvariable.php';
+			include 'ContentVariable.php';
 			echo fread($myFile24,filesize("Content_Details/E0324.html"));
 			?>
 		</div>
 		<div class="Linear_Equations_in_One_Variable-Reducing_equations_to_simpler_form">
 		<?php
-			include 'Contentvariable.php';
+			include 'ContentVariable.php';
 			echo fread($myFile25,filesize("Content_Details/E0325.html"));
 			?>
 		</div>
 
 		<div class="Understanding_Quadrilaterals-Introduction">
 		<?php
-			include 'Contentvariable.php';
+			include 'ContentVariable.php';
 			echo fread($myFile31,filesize("Content_Details/E0331.html"));
 			?>
 		</div>
 		<div class="Understanding_Quadrilaterals-Curves">
 		<?php
-			include 'Contentvariable.php';
+			include 'ContentVariable.php';
 			echo fread($myFile32,filesize("Content_Details/E0332.html"));
 			?>
 		</div>
 		<div class="Understanding_Quadrilaterals-Polygons">
 		<?php
-			include 'Contentvariable.php';
+			include 'ContentVariable.php';
 			echo fread($myFile33,filesize("Content_Details/E0333.html"));
 			?>
 		</div>
 		<div class="Understanding_Quadrilaterals-Diagonals">
 		<?php
-			include 'Contentvariable.php';
+			include 'ContentVariable.php';
 			echo fread($myFile34,filesize("Content_Details/E0334.html"));
 			?>
 		</div>
 		<div class="Understanding_Quadrilaterals-Angle_sum_property">
 		<?php
-			include 'Contentvariable.php';
+			include 'ContentVariable.php';
 			echo fread($myFile35,filesize("Content_Details/E0335.html"));
 			?>
 		</div>
 		<div class="Understanding_Quadrilaterals-Exterior_angles_sum_property">
 		<?php
-			include 'Contentvariable.php';
+			include 'ContentVariable.php';
 			echo fread($myFile36,filesize("Content_Details/E0336.html"));
 			?>
 		</div>
 		<div class="Understanding_Quadrilaterals-Kinds_of_Quadrilaterals">
 		<?php
-			include 'Contentvariable.php';
+			include 'ContentVariable.php';
 			echo fread($myFile37,filesize("Content_Details/E0337.html"));
 			?>
 		</div>
 		<div class="Understanding_Quadrilaterals-Types_of_Parallelograms">
 		<?php
-			include 'Contentvariable.php';
+			include 'ContentVariable.php';
 			echo fread($myFile38,filesize("Content_Details/E0338.html"));
 			?>
 		</div>
 
-		<div class="foPractical_Geometryurth-Introduction">
+		<div class="Practical_Geometry-Introduction">
 		<?php
-			include 'Contentvariable.php';
+			include 'ContentVariable.php';
 			echo fread($myFile41,filesize("Content_Details/E0341.html"));
 			?>
 		</div>
 		<div class="Practical_Geometry-Constructing_Quadrilateral">
 		<?php
-			include 'Contentvariable.php';
+			include 'ContentVariable.php';
 			echo fread($myFile42,filesize("Content_Details/E0342.html"));
 			?>
 		</div>
 		<div class="Practical_Geometry-Special_Quadrilaterals">
 		<?php
-			include 'Contentvariable.php';
+			include 'ContentVariable.php';
 			echo fread($myFile43,filesize("Content_Details/E0343.html"));
 			?>
 		</div>
